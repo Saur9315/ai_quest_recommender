51982dcb3012273d7807f7db6d78781b02586e64
Saur9315 <saur0893@gmail.com>
2025-01-26 00:50:23 +0500

    auto init 2

--- database.py
+++ database.py
@@ -4,8 +4,9 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-load_dotenv()
-DATABASE_URL = os.getenv("DATABASE_URL")
+# DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.path.join(os.getcwd(), "results.db")
+print(f"Connecting to database: {DATABASE_URL}")
 
 
 def get_connection():
